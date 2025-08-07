9c1e852125f5f842edc45b174c85a55601d76b45
kamaukimani <paulkimys285@gmail.com>
2025-08-07 09:42:45 +0300

    update

--- missing.py
+++ missing.py
@@ -4,13 +4,13 @@
 
 # print(missing_number([3, 0, 1]))  # 2
 
-def missing_number(nums):
-    n = len(nums)
+def missing_number(numbers):
+    n = len(numbers)
     expected_sum = n * (n + 1) // 2
-    actual_sum = sum(nums)
+    actual_sum = sum(numbers)
     missing = expected_sum - actual_sum
 # checks if the number provided have a missing number or not 
-    if missing in nums or missing > n:    
+    if missing in numbers or missing > n:    
         return "No missing number"
     else:
         return missing
